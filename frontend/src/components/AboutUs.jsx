39c48e66a77d09660f7d2811d802dda03f94198b
Paper Gremlin <mahajanuday144@gmail.com>
2023-08-30 13:06:59 +0530

    Update About us (revert-2)

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -12,14 +12,14 @@ export default function AboutUs() {
         <RightGradient />
         <Vector sectionName='about us' />
         <div id="about" className="container-fluid pb-lg-5 p-sm-5 p-3 ms-0 mt-lg-4 card-bg rounded-5 row justify-content-center gap-lg-5 fade-in">
-          <div className="d-none d-lg-block col-lg-6 transparent-background">
+          <div className="d-none d-lg-block col-lg-5 transparent-background">
             <div className='d-flex justify-content-center'>
             <img className="img-fluid pulsing-element" src={aboutUsImg} alt='gif'/>
             </div>
           </div>
-          <div className="col-lg-5 col text-light mt-lg-5 mt-md-3 mt-2 pe-lg-5 text-lg-start">
+          <div className="col-lg-6 col text-light mt-lg-5 mt-md-3 mt-2 pe-lg-5 text-lg-start">
             <h1 className="display-3 fw-semibold heading-font text-lg-start text-center">About us</h1>
-            <p className="mt-4 fs-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            <p className="mt-4 card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
               labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
               aliquip ex ea commodo consequat. </p>
             {/* <p className="fs-5">Et netus et malesuada fames ac turpis egestas maecenas. Facilisis mauris sit amet massa vitae
