4bb0fea2a2400a20a3c394d4ac74c6f2452445b0
Paper Gremlin <mahajanuday144@gmail.com>
2023-09-07 09:55:45 +0530

    minor bug fixes (404 page)

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -11,27 +11,29 @@ export default function NotFound() {
       </div>
       <div className='row justify-content-center'>
         <div className="col-auto row justify-content-center gap-3 me-5">
-          <div className='col-3 pt-5 me-3'>
+          <div className='col-3 me-3' style={{paddingTop: '34px'}}>
             <p className='Style1'>4</p>
           </div>
           <div className='col-3'>
             <p className='Style2'>0</p>
           </div>
-          <div className='col-3' style={{ paddingTop: '4.3vh' }}>
+          <div className='col-3' style={{ paddingTop: '24px' }}>
             <p className='Style3'>4</p>
           </div>
         </div>
       </div>
-      <div className='text-center'>
-        <p className='Style4'>oops...!</p>
-        <p className='pt-3 Style5'>Page not found</p>
-        <p className='Style6'>We are sorry but this page doesn't exist :(</p>
-      </div>
-      <div className="d-flex justify-content-center">
-        <a href="/" className="btn button-back-to-homepage btn-md rounded-pill mt-3 text-uppercase py-3 px-4">Return to home page</a>
+      <div className='msg-n-btn'>
+        <div className='text-center'>
+          <p className='Style4'>oops...!</p>
+          <p className='Style5'>Page not found</p>
+          <p className='Style6'>We are sorry but this page doesn't exist :(</p>
+        </div>
+        <div className="d-flex justify-content-center">
+          <a href="/" className="btn button-back-to-homepage btn-md rounded-pill mt-3 text-uppercase py-3 px-4">Return to home page</a>
+        </div>
       </div>
       <div className="not-found-right-gradient">
-      <RightGradient />
+        <RightGradient />
       </div>
     </div>
   )
